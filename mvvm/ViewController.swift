16f766be46004772d70f0ba0634298984b03e814
Spiridonov Maxim <taijased@gmail.com>
2019-10-21 13:20:46 +0300

    Initial Commit

---
+++ mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by Максим Спиридонов on 21.10.2019.
+//  Copyright © 2019 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
