e25c9e459c97bb31ae104cac0b6b45bcb6ef85a0
Spiridonov Maxim <taijased@gmail.com>
2020-02-21 15:42:52 +0300

    add datafetcher

---
+++ mvvm/Service/DataFetcherService.swift
@@ -0,0 +1,25 @@
+//
+//  DataFetcherService.swift
+//  mvvm
+//
+//  Created by Максим Спиридонов on 21.02.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import Foundation
+
+class DataFetcherService {
+    var dataFetcher: DataFetcher
+    
+    init(dataFetcher: DataFetcher = NetworkDataFetcher()) {
+        self.dataFetcher = dataFetcher
+    }
+    
+    
+    func fetchRSSAppleMusic(completion: @escaping (RSSModel?) -> Void) {
+        let urlString = "https://rss.itunes.apple.com/api/v1/us/apple-music/hot-tracks/all/20/explicit.json"
+        dataFetcher.fetchGenericJSONData(urlString: urlString, response: completion)
+    }
+    
+    
+}
